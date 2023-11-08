dbc6345ad37fb499541e02311a39a5d56cc6ed9b
xontoloyo <143369114+xontoloyoo@users.noreply.github.com>
2023-11-08 15:42:24 +0700

    Update nih.py

--- nih.py
+++ nih.py
@@ -782,8 +782,8 @@ with gr.Blocks(title="Nih Cuy") as app:
                 "step1: 填写实验配置. 实验数据放在logs下, 每个实验一个文件夹, 需手工输入实验名路径, 内含实验配置, 日志, 训练得到的模型文件. "
             )
         )
-                    with gr.Row():
-                exp_dir1 = gr.Textbox(label=i18n("输入实验名"), value="mi-test")
+        with gr.Row():
+            exp_dir1 = gr.Textbox(label=i18n("输入实验名"), value="mi-test")
                 sr2 = gr.Radio(
                     label=i18n("目标采样率"),
                     choices=["40k", "48k"],
