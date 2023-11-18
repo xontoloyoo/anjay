f0225d0f25fc39b01cab414b9351b8881671b9f5
xontoloyo <143369114+xontoloyoo@users.noreply.github.com>
2023-11-18 22:08:22 +0700

    Update extract_f0_rmvpe.py

--- infer/modules/train/extract/extract_f0_rmvpe.py
+++ infer/modules/train/extract/extract_f0_rmvpe.py
@@ -31,7 +31,7 @@ def printt(strr):
 
 
 class FeatureInput(object):
-    def __init__(self, samplerate=16000, hop_size=320):
+    def __init__(self, samplerate=16000, hop_size=160):
         self.fs = samplerate
         self.hop = hop_size
 
